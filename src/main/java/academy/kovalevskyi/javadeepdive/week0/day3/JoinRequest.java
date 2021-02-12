4798c4c4fe5e6ece00694689fa6a44d7a3e80b08
Vladyslav <ecstasy.awesome@gmail.com>
2021-02-12 12:59:21 +0200

    update w0d3

--- src/main/java/academy/kovalevskyi/javadeepdive/week0/day3/JoinRequest.java
+++ src/main/java/academy/kovalevskyi/javadeepdive/week0/day3/JoinRequest.java
@@ -1,29 +1,65 @@
 package academy.kovalevskyi.javadeepdive.week0.day3;
 
 import academy.kovalevskyi.javadeepdive.week0.day2.CSV;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.IntStream;
 
 public class JoinRequest extends AbstractRequest<CSV> {
 
-  private final CSV on;
+  private final CSV from;
   private final String by;
 
-  private JoinRequest(CSV from, CSV on, String by) {
-    super(from);
-    this.on = on;
+  private JoinRequest(CSV on, CSV from, String by) {
+    super(on);
+    this.from = from;
     this.by = by;
   }
 
   @Override
   protected CSV execute() throws RequestException {
-    if (!on.withHeader() || !csv.withHeader() || on.values().length != csv.values().length) {
+    if (!from.withHeader() || !csv.withHeader() || from.values().length != csv.values().length) {
       throw new IllegalArgumentException();
     }
-    final var csvColumn = getColumnId(csv, by);
-    final var onColumn = getColumnId(on, by);
-    return null;
+    final var columns = uniqueColumns();
+    var header = join(csv.header(), from.header(), columns);
+    var values = IntStream
+        .range(0, csv.values().length)
+        .mapToObj(index -> join(csv.values()[index], from.values()[index], columns))
+        .toArray(String[][]::new);
+    return new CSV.Builder().header(header).values(values).build();
   }
 
+  private int[] uniqueColumns() throws RequestException {
+    final var csvHeader = Arrays.asList(csv.header());
+    checkExistingColumn(csvHeader);
+    checkExistingColumn(Arrays.asList(from.header()));
+    final var result = IntStream
+        .range(0, from.header().length)
+        .dropWhile(index -> csvHeader.contains(from.header()[index]))
+        .toArray();
+    if (result.length == 0) {
+      throw new RequestException("Csv has no unique columns!");
+    }
+    return result;
+  }
+
+  private void checkExistingColumn(final List<String> header) throws RequestException {
+    if (!header.contains(by)) {
+      throw new RequestException("Common field is absent!");
+    }
+  }
+
+  private static String[] join(final String[] target, final String[] from, final int[] columns) {
+    final var result = new String[target.length + columns.length];
+    System.arraycopy(target, 0, result, 0, target.length);
+    var resultIndex = target.length;
+    for (var index : columns) {
+      result[resultIndex++] = from[index];
+    }
+    return result;
+  }
 
   public static class Builder {
 
@@ -50,7 +86,7 @@ public class JoinRequest extends AbstractRequest<CSV> {
       Objects.requireNonNull(from);
       Objects.requireNonNull(on);
       Objects.requireNonNull(by);
-      return new JoinRequest(from, on, by);
+      return new JoinRequest(from, on, by); // TODO swap from and on after test has been changed
     }
   }
 
